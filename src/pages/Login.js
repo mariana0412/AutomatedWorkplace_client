c7336d68a2fbc5c4f4f39c31e1b680cbb6930a54
Liliia Parashchak <liliia.parashchak@gmail.com>
2023-06-28 22:05:15 +0300

    Login page

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,11 +1,67 @@
+import React, { useState } from 'react';
+import MD5 from "crypto-js/md5";
 import AppNavbar from "../components/AppNavbar";
+import {useNavigate} from "react-router-dom";
 
 const Login = () => {
-    return (
-        <div>
-            <AppNavbar/>
+    const navigate = useNavigate();
+    const [login, setLogin] = useState('');
+    const [password, setPassword] = useState('');
+
+    const handleLogin = async () => {
+        try {
+            const hashedPassword = MD5(password).toString();
+            const response = await fetch(`/login?login=${login}&password=${hashedPassword}`, {
+            method: 'POST',
+        });
+        if (response.status === 200) {
+            const data = await response.json();
+            const token = data.token;
+            localStorage.setItem('token', token);
+            navigate('/');
+        } else {
+            alert("Неправильний логін або пароль.");
+        }
+    } catch (error) {
+        console.error(error);
+    }
+};
+
+return (
+    <div>
+        <AppNavbar />
+        <div className="container d-flex justify-content-center align-items-center">
+            <div className="card w-50 mt-5">
+                <div className="card-body">
+                    <h2 className="mt-4 card-title text-center">Вхід</h2>
+                    <form>
+                        <div className="form-group">
+                            <label>Логін:</label>
+                            <input
+                                type="text"
+                                value={login}
+                                onChange={(e) => setLogin(e.target.value)}
+                                className="form-control"
+                            />
+                        </div>
+                        <div className="form-group">
+                            <label>Пароль:</label>
+                            <input
+                                type="password"
+                                value={password}
+                                onChange={(e) => setPassword(e.target.value)}
+                                className="form-control"
+                            />
+                        </div>
+                        <button type="button" onClick={handleLogin} className="btn btn-primary mt-3">
+                            Увійти
+                        </button>
+                    </form>
+                </div>
+            </div>
         </div>
-    )
-}
+    </div>
+);
+};
 
 export default Login;
