c43cdc3127073816b9bff9e7a0fc20b6366ff4e9
Liliia Parashchak <liliia.parashchak@gmail.com>
2023-06-28 22:06:28 +0300

    Update navbar

--- src/components/AppNavbar.js
+++ src/components/AppNavbar.js
@@ -1,35 +1,49 @@
 import React, { useState } from 'react';
-import {Collapse, Nav, Navbar, NavbarBrand, NavbarToggler} from 'reactstrap';
+import { Collapse, Nav, Navbar, NavbarBrand, NavbarToggler } from 'reactstrap';
 import { Link } from 'react-router-dom';
 
 const AppNavbar = () => {
-
     const [isOpen, setIsOpen] = useState(false);
+
+    const checkToken = () => {
+        const token = localStorage.getItem('token');
+        return !!token;
+    };
+
+    const handleLogout = () => {
+        localStorage.removeItem('token');
+    };
 
     return (
         <Navbar color="dark" dark expand="md">
             <NavbarBrand tag={Link} to="/">
                 Головна
             </NavbarBrand>
-            <NavbarToggler onClick={() => { setIsOpen(!isOpen) }}/>
+            <NavbarToggler onClick={() => setIsOpen(!isOpen)} />
             <Collapse isOpen={isOpen} navbar>
-                <Nav className="justify-content-start" style={{ width: "100%" }} navbar>
-
-                    <NavbarBrand tag={Link} to="/goods">
-                        Товари
-                    </NavbarBrand>
-
-                    <NavbarBrand tag={Link} to="/groups">
-                        Групи
-                    </NavbarBrand>
-
+                <Nav className="justify-content-start" style={{ width: '100%' }} navbar>
+                    {checkToken() && (
+                        <>
+                            <NavbarBrand tag={Link} to="/goods">
+                                Товари
+                            </NavbarBrand>
+                            <NavbarBrand tag={Link} to="/groups">
+                                Групи
+                            </NavbarBrand>
+                        </>
+                    )}
                 </Nav>
 
-                {/*when auth is ready, toggle login and logout depends on token presence*/}
-                <Nav className="justify-content-end" style={{ width: "100%" }} navbar>
-                    <NavbarBrand tag={Link} to="/login">
-                        Увійти
-                    </NavbarBrand>
+                <Nav className="justify-content-end" style={{ width: '100%' }} navbar>
+                    {checkToken() ? (
+                        <NavbarBrand onClick={handleLogout} tag={Link} to="/">
+                            Вийти
+                        </NavbarBrand>
+                    ) : (
+                        <NavbarBrand tag={Link} to="/login">
+                            Увійти
+                        </NavbarBrand>
+                    )}
                 </Nav>
 
             </Collapse>
