a7e0ce413b16c503ce66d659afb953fc0f50db3a
Liliia Parashchak <liliia.parashchak@gmail.com>
2023-06-29 21:23:33 +0300

    Home page styling

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,10 +1,14 @@
 import AppNavbar from '../components/AppNavbar';
-
+import './Home.css';
 const Home = () => {
 
     return (
-        <div>
+        <div className="page">
             <AppNavbar/>
+            <div className="container">
+                <div className="title">АВТОМАТИЗОВАНЕ РОБОЧЕ МІСЦЕ</div>
+                <div className="authors">Автори: Мар'яна Пізь, Лілія Паращак</div>
+            </div>
         </div>
     );
 };
