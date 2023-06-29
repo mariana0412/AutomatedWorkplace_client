db2407b40b3f2ae69f5c97fadaf77a8cb1c31d08
Mariana Piz <marianapiz0412@gmail.com>
2023-06-29 11:58:53 +0300

    Implement filtering goods by group and name

--- src/pages/Goods/Goods.js
+++ src/pages/Goods/Goods.js
@@ -1,15 +1,34 @@
 import AppNavbar from "../../components/AppNavbar";
-import { Button, Container, Table, Modal, ModalHeader, ModalBody, ModalFooter } from 'reactstrap';
+import {Button, Container, Table, Modal, ModalHeader, ModalBody, ModalFooter, FormGroup, Input} from 'reactstrap';
 import './Goods.css';
 import useGoods from "../../hooks/useGoods";
 import useDeleteConfirmation from "../../hooks/useDeleteConfirmation";
 import GoodRow from "./GoodRow";
 import {Link} from "react-router-dom";
+import {useState} from "react";
 
 const Goods = () => {
 
-    const { goods, groups, showEmpty, updateGoodsAfterDeletion } = useGoods();
+    const { goods, groups, showEmpty, updateGoodsAfterDeletion, fetchGoodsByGroup } = useGoods();
     const { deleteGoodModal, toggleModal, handleDelete, confirmDelete } = useDeleteConfirmation(updateGoodsAfterDeletion);
+    const [selectedGroup, setSelectedGroup] = useState("");
+    const [selectedName, setSelectedName] = useState("");
+
+    const handleFilterChange = async (newGroupId, newName) => {
+        await fetchGoodsByGroup(newGroupId, newName);
+    };
+
+    const handleGroupChange = (e) => {
+        const newGroupId = e.target.value;
+        setSelectedGroup(newGroupId);
+        handleFilterChange(newGroupId, selectedName);
+    };
+
+    const handleNameChange = (e) => {
+        const newName = e.target.value;
+        setSelectedName(newName);
+        handleFilterChange(selectedGroup, newName);
+    };
 
     const goodsList = goods.map(good =>
         <GoodRow
@@ -28,9 +47,29 @@ const Goods = () => {
                     Додати
                 </Button>
                 <div className="row">
+
                     <div className="filters">
-                        sdfsdfsdfds
+                        <FormGroup>
+                            <Input type="select" value={selectedGroup} onChange={handleGroupChange}>
+                                <option value="">Усі групи</option>
+                                {groups.map((group) => (
+                                    <option key={group.id} value={group.id}>
+                                        {group.name}
+                                    </option>
+                                ))}
+                            </Input>
+                        </FormGroup>
+
+                        <FormGroup>
+                            <Input
+                                type="text"
+                                placeholder="Назва"
+                                value={selectedName}
+                                onChange={handleNameChange}
+                            />
+                        </FormGroup>
                     </div>
+
                     <div className="goods-table">
                         <Table className="mt-4">
                             <thead>
